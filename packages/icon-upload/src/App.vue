9ee80cbf95adab26ab523f57fe9b2587232f5c80
Mr-Nobody-li <lpf13015886562@gmail.com>
2022-08-04 09:16:41 +0800

    feat: icon upload

---
+++ packages/icon-upload/src/App.vue
@@ -0,0 +1,14 @@
+<!--
+ * @Author: Mr-Nobody-li
+ * @Date: 2022-08
+ * @LastEditors: Mr-Nobody-li
+ * @LastEditTime: 2022-08
+ * @Description: app.vue
+-->
+<script setup lang="ts"></script>
+
+<template>
+  <div>1</div>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ packages/icon-upload/src/vite-env.d.ts
@@ -0,0 +1,15 @@
+/*
+ * @Author: Mr-Nobody-li
+ * @Date: 2022-08
+ * @LastEditors: Mr-Nobody-li
+ * @LastEditTime: 2022-08
+ * @Description: 全局dts
+ */
+/* eslint-disable */
+/// <reference types="vite/client" />
+
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}

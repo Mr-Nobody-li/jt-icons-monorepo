7617001e54e8c4c7870ad7cd443584180ae709cb
Mr-Nobody-li <lpf13015886562@gmail.com>
2022-08-19 11:25:41 +0800

    fix: localhost > 0.0.0.0

--- packages/icon-upload/vite.config.ts
+++ packages/icon-upload/vite.config.ts
@@ -12,7 +12,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
-    host: 'localhost',
+    host: '0.0.0.0',
     port: 3001,
     proxy: {
       '/api': {
